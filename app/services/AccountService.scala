ac23630f5ad3d30c8c1913f60a3a3fd7d468bf19
pongthep <mr.pongthep@gmail.com>
2015-05-20 01:34:31 +0100

    Improve service layer and model layer

--- app/services/AccountService.scala
+++ app/services/AccountService.scala
@@ -1,34 +1,22 @@
 package services
 
-import models._
+import models.Account._
 import scala.slick.driver.H2Driver.simple._
 import slick.driver.PostgresDriver
 import DatabaseService._
 
 object AccountService{
 
-	val account: TableQuery[Accounts] = TableQuery[Accounts]
-
 	def addAccount(name:String,email:String,mobile:String):Option[Account] = {
 		db.withSession { implicit session =>
-			//create database
-			account.ddl.create
-			
-			// insert account instances
-			account.map(a => (a.name, a.email,a.mobile)) += (name,email,mobile)
-			Some(account.filter(_.email === email).first)
+			insertAccount(name,email,mobile)
 		}
 	}
 
 	//Using this form when use postgresql
-	/*def addAccount(acc:AccountPOSTRequest) = {
-	
-		val db = Database.forURL("jdbc:postgresql://localhost/thjug?user=postgres&password=q1w2e3r4maniacgroup", driver = "org.postgresql.Driver")
+	/*def addAccount(name:String,email:String,mobile:String):Option[Account] = {
 		db.withSession { implicit session =>
-	
-			account.map(a => (a.name, a.email,a.mobile)) += (acc.name,acc.email,acc.mobile)
-	
-			println(account.list)
+			insertAccount(name,email,mobile)
 		}
 	}*/
 }
--- app/services/DatabaseService.scala
+++ app/services/DatabaseService.scala
@@ -4,4 +4,5 @@ import scala.slick.driver.H2Driver.simple._
 
 object DatabaseService{
 	val db = Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver")
+	//val db = Database.forURL("jdbc:postgresql://localhost/thjug?user=postgres&password=q1w2e3r4maniacgroup", driver = "org.postgresql.Driver")
 }
