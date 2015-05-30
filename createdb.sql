d6bbe7962223682d5ca792c12299936259815327
nuboat <nuboat@gmail.com>
2015-05-31 00:43:09 +0700

    ReFormat

--- createdb.sql
+++ createdb.sql
@@ -1,10 +1,20 @@
 CREATE SEQUENCE userid_seq;
 
-CREATE TABLE account(
+CREATE TABLE account
+(
 	userid bigint NOT NULL UNIQUE default nextval('userid_seq'),
 	username VARCHAR(256) NOT NULL UNIQUE,
 	email VARCHAR(128) NOT NULL,
 	mobile VARCHAR(16) NOT NULL,
 	level integer,
 	CONSTRAINT account_userid_pk PRIMARY KEY (userid)
+);
+
+CREATE TABLE subscriber
+(
+	email 	    VARCHAR(256) NOT NULL,
+	fullname    VARCHAR(128) NOT NULL,
+	mobile 	    VARCHAR(16) NOT NULL,
+	joblevel    integer   NOT NULL,
+	CONSTRAINT subscriber_pk PRIMARY KEY (email)
 );
